7bb5422d7f60ca80982bbc1df15fa4f04d508407
YC酱luyancib <68143180+luyanci@users.noreply.github.com>
2024-08-22 22:04:58 +0800

    Initial commit

---
+++ PluginTemplate.py
@@ -0,0 +1,30 @@
+flora_api = {}  # 顾名思义,FloraBot的API,载入(若插件已设为禁用则不载入)后会赋值上
+
+
+def occupying_function(*values):  # 该函数仅用于占位,并没有任何意义
+    pass
+
+
+send_msg = occupying_function
+
+
+def init():  # 插件初始化函数,在载入(若插件已设为禁用则不载入)或启用插件时会调用一次,API可能没有那么快更新,可等待,无传入参数
+    global send_msg
+    print(flora_api)
+    send_msg = flora_api.get("SendMsg")
+    print("FloraBot插件模板 加载成功")
+
+
+def api_update_event():  # 在API更新时会调用一次(若插件已设为禁用则不调用),可及时获得最新的API内容,无传入参数
+    print(flora_api)
+
+
+def event(data: dict):  # 事件函数,FloraBot每收到一个事件都会调用这个函数(若插件已设为禁用则不调用),传入原消息JSON参数
+    print(data)
+    uid = data.get("user_id")  # 事件对象QQ号
+    gid = data.get("group_id")  # 事件对象群号
+    mid = data.get("message_id")  # 消息ID
+    msg = data.get("raw_message")  # 消息内容
+    if msg is not None:
+        msg = msg.replace("&#91;", "[").replace("&#93;", "]").replace("&amp;", "&").replace("&#44;", ",")  # 消息需要将URL编码替换到正确内容
+        print(uid, gid, mid, msg)
